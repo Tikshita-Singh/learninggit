e1a7d60ad6b2a369e3283ab94264782886846ddc
Tikshita Singh <96276437+Tikshita-Singh@users.noreply.github.com>
2022-10-05 02:09:53 +0530

    Create Permutation.java

---
+++ Recursion/Permutation.java
@@ -0,0 +1,24 @@
+public class Permutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String p = " ";
+		String up = "abc";
+		permutations(p , up);
+		
+
+	}
+	static void permutations(String p, String up) {
+		if(up.isEmpty()) {
+			System.out.println(p);
+			return;
+		}
+		char ch = up.charAt(0);
+		for(int i=0;i<=p.length();i++) {
+			String f =p.substring(0,i);
+			String s= p.substring(i,p.length());
+			permutations(f+ch +s,up.substring(1));
+		}
+	}
+
+}
